24827ef0fafe03848a0b0c2fd9b5fa368ba4bf24
Umer Amjad - H263353 <muhammadumer.amjad@honeywell.com>
2018-03-31 12:30:48 -0400

    Wrote modular integer multiplicative inverse function using Extended Euclidean Algorithm

--- ZmodP.hpp
+++ ZmodP.hpp
@@ -90,6 +90,22 @@ public:
         return copy;
     }
     
+    Zmod inverse(){
+        //Using the Extended Euclidean Algorithm
+        int r = p;
+        int r_next = elem;
+        int t = 0;
+        int t_next = 1;
+        while (r_next != 0){
+            int quotient = r / r_next;
+            r = r_next;
+            t = t_next;
+            int t_next = t - quotient * t_next;
+            int r_next = r - quotient * r_next;
+        }
+        return 0;
+    }
+    
     friend std::ostream& operator<<(std::ostream& out, const Zmod& val){
         out << val.elem;
         return out;
